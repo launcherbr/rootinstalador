a024af156dbc0d8250a90e972124700a4ef3c541
licencas.digital <131886545+licencasdigital@users.noreply.github.com>
2023-07-18 03:07:33 -0300

    Update _backend.sh

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -12,6 +12,12 @@ backend_redis_create() {
   printf "\n\n"
 
   sleep 2
+
+  sudo su - root <<EOF
+  usermod -aG docker deploy
+  docker run --name redis-${instancia_add} -p ${redis_port}:6379 --restart always --detach redis redis-server --requirepass ${mysql_root_password}
+
+  sleep 2
   sudo su - postgres <<EOF
     createdb ${instancia_add};
     psql
