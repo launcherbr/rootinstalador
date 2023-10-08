ed8785d3e19284ef585a4a948775837b316cfda1
Leandro Palmeira <leandropalmeira@hotmail.com>
2023-10-08 13:39:27 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -288,7 +288,7 @@ system_node_install() {
   sleep 2
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-  sudo apt-get update -y && sudo apt-get -y install postgresql
+  sudo apt-get update -y && sudo apt-get -y install postgresql-15
   sleep 2
   sudo timedatectl set-timezone America/Sao_Paulo
   
